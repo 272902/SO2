4ce158e4952487dd0d0fd59b0d81b544bf86ea99
272902 <123720319+272902@users.noreply.github.com>
2025-03-26 01:08:24 +0100

    Add files via upload
    Main commit 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
 #include <chrono>
 #include <semaphore>
 #include <cstdlib>
@@ -23,10 +24,23 @@ int main() {
         return 1;
     }
 
+    // Mutex forks
+    vector<mutex> forks(numPhilosophers);
+
     const int numIterations = 5; // iteration number (1 iteration - thinking, eating)
 
     // Semaphore with plihosophers number
     counting_semaphore<> table(numPhilosophers - 1);
+
+    // Threads for philosophers
+    vector<thread> philosophers;
+
+    for (int i = 0; i < numPhilosophers; ++i) {
+        philosophers.emplace_back(philosopher, i, numIterations,
+                                  ref(forks[i]),
+                                  ref(forks[(i + 1) % numPhilosophers]),
+                                  ref(table));
+    }
 
     return 0;
 }
