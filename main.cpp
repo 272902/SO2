424d43835a849fc3b3e84f501244fcefc2134741
272902 <123720319+272902@users.noreply.github.com>
2025-03-30 04:10:38 +0200

    Update main.cpp
    chyba dziala ale jest 4:01 wiec moge miec juz halucynacje

--- main.cpp
+++ main.cpp
@@ -8,26 +8,61 @@
 
 using namespace std;
 
-void philosopher(int id, mutex& leftFork, mutex& rightFork, counting_semaphore<>& table) {
+mutex printMutex;
+
+void eat(int id, mutex& mo) {
+    {
+        lock_guard<mutex> lock(mo);
+        cout << "\t\t\t\t\t\t\t\tPhilosopher " << id << " is eating." << endl;
+    }
+    //Eating for a duration of random time (between 1000 and 2000 ms)
+    this_thread::sleep_for(chrono::milliseconds(1000 + rand() % 2000));
+}
+
+void think(int id, mutex& mo) {
+    {
+        lock_guard<mutex> lock(mo);
+        cout << "Philosopher " << id << " is thinking." << endl;
+    }
+    //Thinking for a duration of random time (between 1000 and 2000 ms)
+    this_thread::sleep_for(chrono::milliseconds(1000 + rand() % 2000));
+}
+
+void is_hungry(int id, mutex& mo) {
+    {
+        lock_guard<mutex> lock(mo);
+        cout << "\t\t\tPhilosopher " << id << " is hungry." << endl;
+    }
+    //Thinking for a duration of random time (between 1000 and 2000 ms)
+    this_thread::sleep_for(chrono::milliseconds(1000 + rand() % 2000));
+}
+
+void philosopher(int id, mutex& leftFork, mutex& rightFork, counting_semaphore<>& table, mutex& mo) {
     while(true) {
-        think(id);
-        eat(id);
+        while (true) {
+            think(id, mo);
+            is_hungry(id, mo);
+            table.acquire();
+    
+            if (id % 2 == 0) {
+                lock_guard<mutex> rightLock(rightFork);
+                lock_guard<mutex> leftLock(leftFork);
+                eat(id, mo);
+            } else {
+                lock_guard<mutex> leftLock(leftFork);
+                lock_guard<mutex> rightLock(rightFork);
+                eat(id, mo);
+            }
+            
+            table.release();
+        }
+
+        table.release(); // Philosopher finishes eating
     }
 }
 
-void eat(int id) {
-    cout << "Philosopher " << id << " is eating." << endl;
-    //Eating for a duration of random time (between 100 and 300 ms)
-    this_thread::sleep_for(chrono::milliseconds(100 + rand() % 200));
-}
-
-void think(int id) {
-    cout << "Philosopher " << id << " is thinking." << endl;
-    //Thinking for a duration of random time (between 100 and 300 ms)
-    this_thread::sleep_for(chrono::milliseconds(100 + rand() % 200));
-}
-
 int main(int argc, char* argv[]) {
+    srand(time(nullptr));
 
     int numPhilosophers = 5; // default philosophers number
 
@@ -38,7 +73,7 @@ int main(int argc, char* argv[]) {
             return 1;
         }
     }
-
+    
     // Mutex forks
     vector<mutex> forks(numPhilosophers);
 
@@ -48,11 +83,14 @@ int main(int argc, char* argv[]) {
     // Threads for philosophers
     vector<thread> philosophers;
 
+    mutex mo;
+
     for (int i = 0; i < numPhilosophers; ++i) {
-        philosophers.emplace_back(philosopher, i, numIterations,
+        philosophers.emplace_back(philosopher, i,
                                   ref(forks[i]),
                                   ref(forks[(i + 1) % numPhilosophers]),
-                                  ref(table));
+                                  ref(table),
+                                  ref(mo));
     }
 
     for (auto &t : philosophers) {
